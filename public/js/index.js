a91c166a6526702f8ecd1accb9d509d290db45af
futabaken <who_is_this_king_of_glory@yahoo.co.jp>
2021-05-20 10:51:23 +0900

    ドラッグ移動を諦めてcmanObjMove_v091.jsを削除 radioボタンで追加するセルを選ぶこととする

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,41 @@
 $(function () {
 
-    $('#dropItem1').on('click', function(){
+    $('#dropItem1').on('click', function () {
+        // TODO:削除予定
         positionText();
     });
 
-    var positionText = function (){
-        var left = $('#dropItem1').css('left');
-        $('#saveItem1').val(left);
+    var positionText = function () {
+        // 関数廃止
     }
 
-    $('#addTsk').on('click', function(){
+    $('#btnAddTsk').on('click', function () {
         addTsk();
     })
 
-    var addTsk = function(){
+    var addTsk = function () {
+        var option = $('input[name="heavyRadios"]:checked').val();
+        switch (option) {
+            case "option1":
+                var id = "heavyAndUrgent";
+                break;
+            case "option2":
+                var id = "heavyAndUnurgent";
+                break;
+            case "option3":
+                var id = "unheavyAndUrgent";
+                break;
+            case "option4":
+                var id = "unheavyAndUnurgent";
+                break;
+        };
         var addText = $('#addText').val();
-        $("<div>",{
+        
+        // 新しいタグを作る
+        $("<div>", {
             id: 'item2',
             text: addText,
-            class: 'movearea bg-warning rounded-lg p-2',
-            cmanOMat: 'movearea'
-        }).appendTo('#heavyMap');
+            class: 'bg-warning rounded-lg p-2'
+        }).appendTo('#' + id);
     }
 });
