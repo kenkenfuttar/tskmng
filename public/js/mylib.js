ea6cff0a2e4991c08d7c187385e19a946dd73291
futabaken <who_is_this_king_of_glory@yahoo.co.jp>
2021-05-26 14:55:53 +0900

    jestをテスト導入

---
+++ public/js/mylib.js
@@ -0,0 +1,8 @@
+export function add(a, b) {
+    return a + b;
+};
+
+function subb(a, b) {
+    return a - b;
+}
+subb;
---
+++ test/mylib.test.js
@@ -0,0 +1,16 @@
+import { add, sub } from '../public/js/mylib';
+
+test('add 1 + 2', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('subb 1 - 2', () => {
+  expect(subb(1, 2)).toBe(-1);
+});
+
+const subb = require('../public/js/mylib').__get__('subb');
+describe('subb', () => {
+  it('subb 1 - 2', () => {
+    expect(subb(1, 2)).toEqual(-1);
+  });
+});
